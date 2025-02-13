627d58bf071f292e0159a0c47a29e422f97add56
clohonauman <charlie.adi01@gmail.com>
2025-02-13 21:57:29 +0800

    Add: Security to access root folder

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+function loadEnv($file)
+{
+    if (!file_exists($file)) {
+        return;
+    }
+
+    $lines = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos($line, '=') !== false) {
+            list($key, $value) = explode('=', $line, 2);
+            $key = trim($key);
+            $value = trim($value);
+            putenv("$key=$value");
+            $_ENV[$key] = $value;
+            $_SERVER[$key] = $value;
+        }
+    }
+}
+loadEnv(__DIR__ . '/.env');
+header('Location: ' . getenv('BASE_URL'));
+exit;
