8898ac55ef7173e9395e13d34b47c5c5e64bca05
clohonauman <charlie.adi01@gmail.com>
2025-02-15 10:14:32 +0800

    Add: Pembelajaran controller

---
+++ controllers/PembelajaranController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class PembelajaranController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
