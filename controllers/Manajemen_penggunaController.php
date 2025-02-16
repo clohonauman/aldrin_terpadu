03c506ed1e3a476e5f57e461e0013e0b3f692519
clohonauman <charlie.adi01@gmail.com>
2025-02-16 18:18:48 +0800

    Add: Manajemen Pengguna Controller

---
+++ controllers/Manajemen_penggunaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class Manajemen_penggunaController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
---
+++ views/manajemen_pengguna/index.php
@@ -0,0 +1,27 @@
+<?php
+/** @var yii\web\View $this */
+$this->title='MANAJEMEN PENGGUNA';
+?>
+<div class="card">
+    <div class="card-header">
+        <div class="col-sm-12">
+            <h1>
+                <i class="fa fa-users-cog icon-title"></i> Manajemen Pengguna
+            </h1>
+        </div>
+    </div>
+    <div class="card-body ">
+        <section class="content">
+            <div class="row">
+                <div class="col-lg-12 col-xs-12">
+                    <div class="card alert alert-danger alert-dismissable">
+                        <p style="font-size:15px">
+                            <b><i class="fa fa-warning"></i> Perhatian!</b><br>
+                            Akses menu ini hanya lewat aplikasi <b>Aldrin Sertifikasi</b> dengan masuk sebagai <b>Super Admin</b>. Terima kasih.
+                        </p>        
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
