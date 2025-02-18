bae24fa624ccfa0acb7c030626f7ee8ab5cd7f3b
clohonauman <charlie.adi01@gmail.com>
2025-02-18 23:17:33 +0800

    Modified: Update layout for Index

--- views/index/index.php
+++ views/index/index.php
@@ -6,14 +6,14 @@ $this->title='ALDRIN TERPADU';
 
 ?>
 <div class="wrapper">
-    <nav class="main-header navbar navbar-expand navbar-danger navbar-danger">
+    <nav class="main-header navbar navbar-expand navbar-primary navbar-primary border-primary">
         <a href="#" class="navbar-brand">
             <img src="<?= Yii::getAlias('@web') ?>/app_logo.png" alt="Logo" class="brand-image w-100">
         </a>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <?= Html::a('Masuk', ['/login'], ['class' => 'btn btn-primary']) ?>
+                    <?= Html::a('Masuk', ['/login'], ['class' => 'btn btn-light']) ?>
                 </li>
             </ul>
         </div>
