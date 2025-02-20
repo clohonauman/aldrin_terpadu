cfadcd8bc22e3f9d2d2a9ed7bbab15e85f2c47d9
clohonauman <charlie.adi01@gmail.com>
2025-02-20 20:33:14 +0800

    Modified: Update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 try {
     return [
         'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=' . '127.0.0.1' . ';port=' .'3306' . ';dbname=' . 'db_aldrin',
-        'username' => 'root',
-        'password' => '',
+        'dsn' => 'mysql:host=' . $_ENV['DB_HOSTNAME'] . ';port=' . $_ENV['DB_PORT'] . ';dbname=' . $_ENV['DB_NAME'],
+        'username' => $_ENV['DB_USERNAME'],
+        'password' => $_ENV['DB_PASSWORD'],
         'charset' => 'utf8',
         // Schema cache options (for production environment)
         //'enableSchemaCache' => true,
